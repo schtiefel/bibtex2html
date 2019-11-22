9dcacae0ba77ce118087a225185ff3c0dbbf37b4
Steve Grogorick <steve@grogorick.de>
2019-11-22 23:57:31 +0100

    added `composer.json\´ and removed WP stuff

--- wp-bibtex-plugin.make-zip.sh
+++
@@ -1,3 +0,0 @@
-# packs the contents of wp-bibtex-plugin-source into a zip file (and dereferences the symbolic link within)
-rm wp-bibtex-plugin.zip
-tar -czhf wp-bibtex-plugin.zip wp-bibtex-plugin-source/
